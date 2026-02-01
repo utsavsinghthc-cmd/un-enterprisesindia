9b990d4e696bd6c82e84bfe52588a8cec225d18f
utsavsinghthc-cmd <utsavsinghthc@gmail.com>
2026-02-01 22:27:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 fetch("products.json")
-.then(r => r.json())
+.then(res => res.json())
 .then(data => {
-  let out = "";
-  data.forEach(p => {
-    out += `
-      <div class="product-card">
-        <img src="${p.image}">
-        <h3>${p.name}</h3>
-        <p>${p.desc}</p>
-      </div>`;
-  });
-  document.getElementById("products").innerHTML = out;
+    let html = "";
+    data.forEach(cat => {
+        html += `
+        <div class="product-card">
+            <img src="${cat.image}">
+            <h3>${cat.category}</h3>
+            <ul>`;
+        cat.items.forEach(item => {
+            html += `<li>${item}</li>`;
+        });
+        html += `
+            </ul>
+            <a href="https://wa.me/919889482011?text=Hello%20UN%20ENTERPRISES,%20I%20want%20details%20about%20${encodeURIComponent(cat.category)}"
+               target="_blank">Enquire on WhatsApp</a>
+        </div>`;
+    });
+    document.getElementById("products").innerHTML = html;
 });
