ade4b06b0306b62f718e982fe4e567ffd3c60c64
utsavsinghthc-cmd <utsavsinghthc@gmail.com>
2026-02-02 20:18:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -68,4 +68,40 @@ function openCart() {
   } else {
     box.innerHTML = cart.map((c, i) => `
       <div class="cart-item">
+        <b>${c.category}</b><br>
+        ${c.price}<br>
+        <button onclick="removeFromCart(${i})">Remove</button>
+      </div>
+    `).join("");
+  }
+  popup.style.display = "block";
+}
 
+function closeCart() {
+  document.getElementById("cartPopup").style.display = "none";
+}
+
+function removeFromCart(i) {
+  cart.splice(i, 1);
+  document.getElementById("cartCount").innerText = cart.length;
+  openCart();
+}
+
+function checkout() {
+  if (cart.length === 0) {
+    alert("Cart is empty");
+    return;
+  }
+
+  let msg =
+    "Hello UN ENTERPRISES,\nGSTIN: 09BFEPS7093M1ZK\n\nI want to enquire about:\n";
+
+  cart.forEach(c => {
+    msg += `- ${c.category} (${c.price})\n`;
+  });
+
+  window.open(
+    "https://wa.me/916386319056?text=" + encodeURIComponent(msg),
+    "_blank"
+  );
+}
