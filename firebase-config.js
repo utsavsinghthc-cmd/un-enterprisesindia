e8eca402ecd22c05fc28df760b9f336112559431
utsavsinghthc-cmd <utsavsinghthc@gmail.com>
2026-02-13 19:47:06 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,32 +1,15 @@
-(function () {
-  const firebaseConfig = {
-    apiKey: "AIzaSyB81eBY1MS5MyTgb1ocCDqtbPDaDboNf_4",
-    authDomain: "un-enterprises.firebaseapp.com",
-    projectId: "un-enterprises",
-    storageBucket: "un-enterprises.firebasestorage.app",
-    messagingSenderId: "1083661598291",
-       appId: "1:1083661598291:web:98a54861b83146e2c490a6",
-    measurementId: "G-3K6XF0ZLWK"
-  };
+var firebaseConfig = {
+  apiKey: "AIzaSyB81eBY1MS5MyTgb1ocCDqtbPDaDboNf_4",
+  authDomain: "un-enterprises.firebaseapp.com",
+  projectId: "un-enterprises",
+  storageBucket: "un-enterprises.firebasestorage.app",
+  messagingSenderId: "1083661598291",
+  appId: "1:1083661598291:web:98a54861b83146e2c490a6"
+};
 
-  if (!window.firebase) {
-    console.error("Firebase SDK failed to load.");
-    return;
-  }
+firebase.initializeApp(firebaseConfig);
 
-  if (!firebase.apps.length) {
-    firebase.initializeApp(firebaseConfig);
-  }
+// 🔥 MAKE DB GLOBAL
+window.db = firebase.firestore();
 
- const services = {
-    auth: firebase.auth(),
-    db: firebase.firestore()
-  };
-
-   if (typeof firebase.analytics === "function") {
-    services.analytics = firebase.analytics();
-  }
-
-  window.firebaseServices = services;
-  console.log("Firebase connected successfully");
-})();
+console.log("Firebase connected successfully");
