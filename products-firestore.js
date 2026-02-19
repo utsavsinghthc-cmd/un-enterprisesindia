91250c27c983f2eb57d648bcd40a4325d5104e22
utsavsinghthc-cmd <utsavsinghthc@gmail.com>
2026-02-19 18:39:43 +0530

    Update products-firestore.js

--- products-firestore.js
+++ products-firestore.js
@@ -7,7 +7,7 @@ function escapeHtml(value) {
     .replace(/&/g, "&amp;")
     .replace(/</g, "&lt;")
     .replace(/>/g, "&gt;")
-    .replace(/"/g, "&quot;")
+    .replace(/\"/g, "&quot;")
     .replace(/'/g, "&#39;");
 }
 
